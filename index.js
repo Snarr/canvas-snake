b647b40d50c3706830c40ed17fa5ec58184f57b4
Jacob Snarr <jacobsnarr@gmail.com>
2021-05-20 05:24:40 -0400

    Standardize Units

--- index.js
+++ index.js
@@ -11,27 +11,29 @@ let gameover = false;
 
 let lastDirection = 0;
 
+let unitSize = 20;
+let windowSize = 40;
+
 let head = {
-  size: 20,
-  speedX: 20,
+  speedX: unitSize,
   speedY: 0,
-  x: 40,
-  y: 20,
+  x: unitSize*2,
+  y: unitSize,
   body: [
     {
-      x: 20,
-      y: 20,
+      x: unitSize,
+      y: unitSize,
     },
     {
       x: 0,
-      y: 20,
+      y: unitSize,
     }
   ]
 }
 
 let apple = {
-  posx: 60,
-  posy: 60,
+  x: unitSize*5,
+  y: unitSize*5,
 }
 
 function keyDown(e) {
@@ -47,7 +49,7 @@ function keyDown(e) {
       console.log("Left arrow");
 
       if (lastDirection !== keys.right) {
-        head.speedX = -head.size;
+        head.speedX = -unitSize;
         head.speedY = 0;
         lastDirection = keys.left;
       }
@@ -57,7 +59,7 @@ function keyDown(e) {
       console.log("Up arrow");
 
       if (lastDirection !== keys.down) {
-        head.speedY = -head.size;
+        head.speedY = -unitSize;
         head.speedX = 0;
         lastDirection = keys.up;
       }
@@ -68,7 +70,7 @@ function keyDown(e) {
       console.log("Right arrow");
 
       if (lastDirection !== keys.left) {
-        head.speedX = head.size;
+        head.speedX = unitSize;
         head.speedY = 0;
         lastDirection = keys.right;
       }
@@ -79,11 +81,10 @@ function keyDown(e) {
       console.log("Down arrow");
 
       if (lastDirection !== keys.up) {
-        head.speedY = head.size;
+        head.speedY = unitSize;
         head.speedX = 0;
         lastDirection = keys.down;
       }
-
 
       break;
   }
@@ -107,21 +108,21 @@ setInterval(() => {
 
   if (gameover) {
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, 1000, 1000);
+    ctx.fillRect(0, 0, windowSize*unitSize, windowSize*unitSize);
     pause = true;
     return;
   }
 
   ctx.fillStyle = 'green';
-  ctx.fillRect(0, 0, 1000, 1000);
+  ctx.fillRect(0, 0, windowSize*unitSize, windowSize*unitSize);
 
-  if (head.x < 0) { head.x = 980 }
-  if (head.x >= 1000) { head.x = 0 }
-  if (head.y < 0) { head.y = 980 }
-  if (head.y >= 1000) { head.y = 0 }
+  if (head.x < 0) { head.x = (windowSize-1)*unitSize }
+  if (head.x >= windowSize*unitSize) { head.x = 0 }
+  if (head.y < 0) { head.y = (windowSize-1)*unitSize }
+  if (head.y >= windowSize*unitSize) { head.y = 0 }
 
   ctx.fillStyle = "black"
-  ctx.fillRect(head.x, head.y, head.size, head.size)
+  ctx.fillRect(head.x, head.y, unitSize, unitSize)
 
   for (var i = head.body.length-1; i >= 0; i--) {
     let part = head.body[i]
@@ -132,7 +133,7 @@ setInterval(() => {
     }
 
     ctx.fillStyle = "black"
-    ctx.fillRect(part.x, part.y, head.size, head.size)
+    ctx.fillRect(part.x, part.y, unitSize, unitSize)
 
     if (i == 0) {
       part.x = head.x;
@@ -143,15 +144,15 @@ setInterval(() => {
     }
   }
 
-  if (apple.posx == head.x && apple.posy == head.y) {
-    apple.posx = 20*Math.floor(Math.random() * 25);
-    apple.posy = 20*Math.floor(Math.random() * 25);
+  if (apple.x == head.x && apple.y == head.y) {
+    apple.x = unitSize*Math.floor(Math.random() * (windowSize-1));
+    apple.y = unitSize*Math.floor(Math.random() * (windowSize-1));
 
     head.body.push({ posx: 0, posy: 0 });
   }
 
   ctx.fillStyle = "red"
-  ctx.fillRect(apple.posx, apple.posy, head.size, head.size)
+  ctx.fillRect(apple.x, apple.y, unitSize, unitSize)
 
   head.x += head.speedX;
   head.y += head.speedY;
